2e88b0b706e636624781e7ff96a246f494320dd4
konstantin <heymailon@gmail.com>
2018-11-11 21:49:08 +0300

    Implement finding of instances on the image, instance object creation closes #2

--- recognition.py
+++ recognition.py
@@ -1,7 +1,18 @@
 from PIL import Image
 
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
 class Instance:
+    def __init__(self, pixels):
+        min_x = min(x for x, y in pixels)
+        min_y = min(y for x, y in pixels)
+
+        self.start_pix = min_x, min_y
+        self.pixels = [(x - min_x, y - min_y) for x, y in pixels]
+
     def classify(self):
         """
         If instance is similar to one of defined letters, makes an attribute 'letter', which is string representation 
@@ -10,6 +21,20 @@ class Instance:
         :return: None
         """
         pass
+
+
+def get_neighbours(pixel):
+    """
+    Find neighbour pixels of 'pixel'
+    :param pixel: (x, y) - tuple of pixel coordinates 
+    :return: set of neighbours 
+    """
+    x_locality = (pixel[0] - 1, pixel[0], pixel[0] + 1)
+    y_locality = (pixel[1] - 1, pixel[1], pixel[1] + 1)
+
+    neighbours = [(x, y) for x in x_locality for y in y_locality]
+    neighbours.remove(pixel)
+    return neighbours
 
 
 def handle_image(img):
@@ -23,11 +48,32 @@ def handle_image(img):
 
 def find_instances(img):
     """
-    Find instances in image
+    Find instances on the image
     :param img: PIL.Image.Image object
     :return: list of Instance objects
     """
-    pass
+    img_data = img.load()
+    size = img.size
+    black_pixels = {
+        (x, y) for x in range(size[0]) for y in range(size[1]) if img_data[x, y] == BLACK
+    }
+
+    instances = list()
+
+    while black_pixels:
+        object_pixels = set()
+        next_pixels = {black_pixels.pop(), }
+
+        while next_pixels:
+            black_pixels -= next_pixels
+            object_pixels.update(next_pixels)
+
+            next_pixels = set().union(*[get_neighbours(p) for p in next_pixels])
+            next_pixels &= black_pixels
+
+        instances.append(Instance(object_pixels))
+
+    return instances
 
 
 def create_output_image(img, instances):
