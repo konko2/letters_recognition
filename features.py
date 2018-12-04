6c0531647ac68a8d72c6d51c424d60b3f6550b77
konstantin <heymailon@gmail.com>
2018-12-04 20:41:42 +0300

    Add offsets to features

--- features.py
+++ features.py
@@ -20,7 +20,7 @@ LETTERS_DETERMINATION = {
     'B': {
         'B_circles': True,
         'left_vertical_line': True,
-        'middle_vertical_line': False,
+        'middle_vertical_line': False
     },
     'C': {
         'C_circle': True,
@@ -29,7 +29,8 @@ LETTERS_DETERMINATION = {
         'middle_vertical_line': False,
         'right_vertical_line': False,
         'A_slopping_lines': False,
-        'B_circles': False
+        'B_circles': False,
+        'D_belly': False
     },
     'D': {
         'D_belly': True,
@@ -47,7 +48,7 @@ LETTERS_DETERMINATION = {
         'middle_vertical_line': False,
         'right_vertical_line': False,
         'A_slopping_lines': False,
-        'B_circles': False,
+        'B_circles': False
     },
     'F': {
         'left_vertical_line': True,
@@ -110,24 +111,49 @@ LETTERS_DETERMINATION = {
 }
 
 
-def has_hook_from_J(instance, *, _show_area=False):
+def has_left_vertical_line(instance, *, _show_area=False):
     """
-    True if instance image has a hook from J
+    True if instance image has a left vertical line.
     :param instance: recognition.Instance object
     :param _show_area: if True shows area of feature for instance
     :return: True or False
     """
     size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    verifying_pixels = {(bbox[0], i) for i in range(bbox[1], bbox[3])}
 
-    start_pix = (0, size[1] * 3 // 5)
-    end_pix = (size[0] * 5 // 8, size[1])
-    middle_pix = ((start_pix[0] + end_pix[0]) / 2, (start_pix[1] + end_pix[1]) / 2)
-    ellipse = get_ellipse_pixels(*start_pix, *end_pix)
+    eps = (size[0] // 6, size[1] // 8)
+    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
-    verifying_pixels = {p for p in ellipse if p[1] >= middle_pix[1]}
 
-    eps = min(size) // 6
+def has_middle_vertical_line(instance, *, _show_area=False):
+    """
+    True if instance image has a middle vertical line.
+    :param instance: recognition.Instance object
+    :param _show_area: if True shows area of feature for instance
+    :return: True or False
+    """
+    size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    middle_x = (bbox[2] + bbox[0]) // 2
+    verifying_pixels = {(middle_x, i) for i in range(bbox[1], bbox[3])}
 
+    eps = (size[0] // 6, size[1] // 8)
+    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
+
+
+def has_right_vertical_line(instance, *, _show_area=False):
+    """
+    True if instance image has a right vertical line.
+    :param instance: recognition.Instance object
+    :param _show_area: if True shows area of feature for instance
+    :return: True or False
+    """
+    size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    verifying_pixels = {(bbox[2] - 1, i) for i in range(bbox[1], bbox[3])}
+
+    eps = (size[0] // 6, size[1] // 8)
     return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
 
@@ -139,13 +165,10 @@ def has_upper_horizont_line(instance, *, _show_area=False):
     :return: True or False
     """
     size = instance.size
-
-    verifying_pixels = {(i, 0) for i in range(size[0])}
-    edges = size[0] // 8, size[0] * 7 // 8
-    verifying_pixels = {p for p in verifying_pixels if edges[0] < p[0] < edges[1]}
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    verifying_pixels = {(i, bbox[1]) for i in range(bbox[0], bbox[2])}
 
     eps = (size[0] // 8, size[1] // 6)
-
     return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
 
@@ -157,13 +180,88 @@ def has_bottom_horizont_line(instance, *, _show_area=False):
     :return: True or False
     """
     size = instance.size
-
-    verifying_pixels = {(i, size[1] - 1) for i in range(size[0])}
-    edges = size[0] // 8, size[0] * 7 // 8
-    verifying_pixels = {p for p in verifying_pixels if edges[0] < p[0] < edges[1]}
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    verifying_pixels = {(i, bbox[3] - 1) for i in range(bbox[0], bbox[2])}
 
     eps = (size[0] // 8, size[1] // 6)
+    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
+
+def has_1_part_horizont_line(instance, *, _show_area=False):
+    """
+    True if instance image has the first part of middle horizontal line.
+    :param instance: recognition.Instance object
+    :param _show_area: if True shows area of feature for instance
+    :return: True or False
+    """
+    size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = ((bbox[2] - bbox[0]) // 2, (bbox[3] - bbox[1]) // 2)
+
+    middle_y = (bbox[1] + bbox[3]) // 2
+    verifying_pixels = {(i, middle_y) for i in range(bbox[0], bbox[0] + bbox_size[0] * 2 // 5)}
+
+    eps = (size[0] // 18, size[1] // 5)
+    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
+
+
+def has_2_part_horizont_line(instance, *, _show_area=False):
+
+    """
+    True if instance image has the second part of middle horizontal line.
+    :param instance: recognition.Instance object
+    :param _show_area: if True shows area of feature for instance
+    :return: True or False
+    """
+    size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = ((bbox[2] - bbox[0]) // 2, (bbox[3] - bbox[1]) // 2)
+
+    middle_y = (bbox[1] + bbox[3]) // 2
+    verifying_pixels = {(i, middle_y) for i in range(bbox[0] + bbox_size[0] * 2 // 5, bbox[0] + bbox_size[0] * 3 // 5)}
+
+    eps = (size[0] // 18, size[1] // 5)
+    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
+
+
+def has_3_part_horizont_line(instance, *, _show_area=False):
+
+    """
+    True if instance image has the third part of middle horizontal line.
+    :param instance: recognition.Instance object
+    :param _show_area: if True shows area of feature for instance
+    :return: True or False
+    """
+    size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = ((bbox[2] - bbox[0]) // 2, (bbox[3] - bbox[1]) // 2)
+
+    middle_y = (bbox[1] + bbox[3]) // 2
+    verifying_pixels = {(i, middle_y) for i in range(bbox[0] + bbox_size[0] * 3 // 5, bbox[2])}
+
+    eps = (size[0] // 18, size[1] // 5)
+    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
+
+
+def has_hook_from_J(instance, *, _show_area=False):
+    """
+    True if instance image has a hook from J
+    :param instance: recognition.Instance object
+    :param _show_area: if True shows area of feature for instance
+    :return: True or False
+    """
+    size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = (bbox[2] - bbox[0], bbox[3] - bbox[1])
+
+    start_pix = (bbox[0], bbox[1] + bbox_size[1] * 3 // 5)
+    end_pix = (bbox[0] + bbox_size[0] * 5 // 8, bbox[3])
+    ellipse = get_ellipse_pixels(*start_pix, *end_pix)
+
+    center_y = (start_pix[1] + end_pix[1]) / 2
+    verifying_pixels = {p for p in ellipse if p[1] >= center_y}
+
+    eps = min(size) // 6
     return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
 
@@ -175,69 +273,12 @@ def has_A_slopping_lines(instance, *, _show_area=False):
     :return: True or False
     """
     size = instance.size
-
-    lines = get_A_slopping_lines_pixels(size[0], size[1])
-    verifying_pixels = {p for p in lines if p[1] <= 5/6*size[1]}
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    verifying_pixels = set(get_A_slopping_lines_pixels(*bbox))
 
     eps = min(size) // 5
 
     return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
-
-
-def _has_part_horizont_line(instance, part_begin_end, *, _show_area=False):
-    """
-    True if instance image has a corresponding quarter of middle horizontal line,
-    where parameter quarter is a sequential number of quarter.
-    :param instance: recognition.Instance object
-    :param quarter_num: integer 1, 2, 3 or 4
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    size = instance.size
-    middle_height = size[1] // 2
-    verifying_pixels = {
-        (x, middle_height) for x in range(int(part_begin_end[0]*size[0]), int(part_begin_end[1]*size[0]) + 1)
-    }
-    edges = size[0] // 6, size[0] * 5 // 6
-    verifying_pixels = {p for p in verifying_pixels if edges[0] < p[0] < edges[1]}
-    instance_pixels = set(instance.pixels)
-
-    eps = (instance.size[0] // 18, instance.size[1] // 5)
-
-    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
-
-
-def has_1_part_horizont_line(instance, *, _show_area=False):
-
-    """
-    True if instance image has the first part of middle horizontal line.
-    :param instance: recognition.Instance object
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    return _has_part_horizont_line(instance, (0, 2/5), _show_area=_show_area)
-
-
-def has_2_part_horizont_line(instance, *, _show_area=False):
-
-    """
-    True if instance image has the second part of middle horizontal line.
-    :param instance: recognition.Instance object
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    return _has_part_horizont_line(instance, (2/5, 3/5), _show_area=_show_area)
-
-
-def has_3_part_horizont_line(instance, *, _show_area=False):
-
-    """
-    True if instance image has the third part of middle horizontal line.
-    :param instance: recognition.Instance object
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    return _has_part_horizont_line(instance, (3/5, 1), _show_area=_show_area)
 
 
 def has_C_circle(instance, *, _show_area=False):
@@ -248,12 +289,14 @@ def has_C_circle(instance, *, _show_area=False):
     :return: True or False
     """
     size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = (bbox[2] - bbox[0], bbox[3] - bbox[1])
 
-    ellipse = get_ellipse_pixels(0, 0, 20 * size[0] // 19, size[1])
-    verifying_pixels = {p for p in ellipse if p[0] < size[0]}
+    ellipse = get_ellipse_pixels(bbox[0], bbox[1], bbox[0] + 20 * bbox_size[0] // 19, bbox[3])
+    verifying_pixels = {p for p in ellipse if p[0] < bbox[2]}
 
-    middle_pix = (size[0] // 2, size[1] // 2)
-    bound = size[1] // 12
+    middle_pix = ((bbox[0] + bbox[2]) // 2, (bbox[1] + bbox[3]) // 2)
+    bound = bbox[1] + bbox_size[1] // 12
     optional_tail = {p for p in verifying_pixels if (
         middle_pix[0] < p[0] and
         bound <= p[1] < middle_pix[1]
@@ -273,19 +316,20 @@ def has_B_circles(instance, *, _show_area=False):
     :return: True or False
     """
     size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = (bbox[2] - bbox[0], bbox[3] - bbox[1])
 
-    a1 = 3 * size[0] // 4
-    ellipse1 = get_ellipse_pixels(0, 0, 2 * a1, size[1] // 2)
-    upper_belly = {(x - a1, y) for x, y in ellipse1 if x >= a1}
+    a1 = 3 * bbox_size[0] // 4
+    ellipse1 = get_ellipse_pixels(bbox[0], bbox[1], bbox[0] + 2 * a1, bbox[1] + bbox_size[1] // 2)
+    upper_belly = {(x - a1, y) for x, y in ellipse1 if x >= bbox[0] + a1}
 
-    a2 = size[0]
-    ellipse2 = get_ellipse_pixels(0, size[1] // 2, 2 * a2, size[1])
-    bottom_belly = {(x - a2, y) for x, y in ellipse2 if x >= a2}
+    a2 = bbox_size[0]
+    ellipse2 = get_ellipse_pixels(bbox[0], bbox[1] + bbox_size[1] // 2, bbox[0] + 2 * a2, bbox[3])
+    bottom_belly = {(x - a2, y) for x, y in ellipse2 if x >= bbox[0] + a2}
 
-    verifying_pixels = {p for p in upper_belly | bottom_belly if p[0] >= size[0] // 10}
+    verifying_pixels = upper_belly | bottom_belly
 
     eps = min(size) // 6
-
     return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
 
@@ -297,66 +341,13 @@ def has_D_belly(instance, *, _show_area=False):
     :return: True or False
     """
     size = instance.size
+    bbox = (size[0] // 8, size[1] // 8, size[0] * 7 // 8, size[1] * 7 // 8)
+    bbox_size = (bbox[2] - bbox[0], bbox[3] - bbox[1])
 
-    ellipse = get_ellipse_pixels(0, 0, 2 * size[0] - 1, size[1])
-    verifying_pixels = {(x - size[0], y) for x, y in ellipse if x >= size[0]}
+    ellipse = get_ellipse_pixels(bbox[0], bbox[1], bbox[0] + 2 * bbox_size[0] - 1, bbox[3])
+    verifying_pixels = {(x - bbox_size[0], y) for x, y in ellipse if x >= bbox[2]}
 
     eps = min(size) // 5
-
-    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
-
-
-def has_left_vertical_line(instance, *, _show_area=False):
-    """
-    True if instance image has a left vertical line.
-    :param instance: recognition.Instance object
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    size = instance.size
-
-    verifying_pixels = {(0, i) for i in range(size[1] + 1)}
-    edges = size[1] // 8, size[1] * 7 // 8
-    verifying_pixels = {p for p in verifying_pixels if edges[0] < p[1] < edges[1]}
-
-    eps = (size[0] // 6, size[1] // 8)
-
-    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
-
-
-def has_middle_vertical_line(instance, *, _show_area=True):
-    """
-    True if instance image has a middle vertical line.
-    :param instance: recognition.Instance object
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    size = instance.size
-
-    verifying_pixels = {(size[0] // 2, i) for i in range(size[1] + 1)}
-    edges = size[1] // 8, size[1] * 7 // 8
-    verifying_pixels = {p for p in verifying_pixels if edges[0] < p[1] < edges[1]}
-
-    eps = (size[0] // 6, size[1] // 8)
-
-    return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
-
-
-def has_right_vertical_line(instance, *, _show_area=False):
-    """
-    True if instance image has a right vertical line.
-    :param instance: recognition.Instance object
-    :param _show_area: if True shows area of feature for instance
-    :return: True or False
-    """
-    size = instance.size
-
-    verifying_pixels = {(size[0] - 1, i) for i in range(size[1])}
-    edges = size[1] // 8, size[1] * 7 // 8
-    verifying_pixels = {p for p in verifying_pixels if edges[0] < p[1] < edges[1]}
-
-    eps = (size[0] // 6, size[1] // 8)
-
     return _scale_feature_with_verifying_pixels(instance, verifying_pixels, eps, _show_area=_show_area)
 
 
